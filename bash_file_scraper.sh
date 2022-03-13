54bf641d87896d4d9af8e9dc386e03d4ade792a5
Yorben Verhaeren <yorbenverhaeren@hotmail.com>
2022-03-13 15:37:44 +0100

    Adding a bash file

---
+++ bash_file_scraper.sh
@@ -0,0 +1,11 @@
+# ---- This is the bash_file_scraper.sh ----
+
+# clearing the window
+clear
+
+# Starting the script
+echo "Starting shell script";
+echo "Ending shell script";
+
+# Printing whats in the bash file
+cat bash_file_scraper.sh
